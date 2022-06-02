e7d13836c1fcf2b6d9375b8bccf07d1860c7bde3
Sebastian Racs <sebastian.racs@itk-engineering.de>
2022-06-02 15:46:32 +0200

    Specify location and resourceGroupName

--- index.ts
+++ index.ts
@@ -2,4 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 
 // Create a separate resource group for this example.
-const resourceGroup = new resources.ResourceGroup("cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg");
+const resourceGroup = new resources.ResourceGroup("resourceGroup", {
+    location: "westeurope",
+    resourceGroupName: "cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg",
+});
