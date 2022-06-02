2e461ffd8f62117b6617b92fbb17a5107865c4b8
Sebastian Racs <sebastian.racs@itk-engineering.de>
2022-06-02 16:00:26 +0200

    Add owner tag

--- index.ts
+++ index.ts
@@ -5,4 +5,5 @@ import * as resources from "@pulumi/azure-native/resources";
 const resourceGroup = new resources.ResourceGroup("resourceGroup", {
     location: "westeurope",
     resourceGroupName: "cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg",
+    tags: {owner: "ras1rlz"},
 });
