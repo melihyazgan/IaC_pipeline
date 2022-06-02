547914f3b3d33abc1856342d19e452546ca227cc
Sebastian Racs <sebastian.racs@itk-engineering.de>
2022-06-02 15:37:43 +0200

    Add package @pulumi/azure-native

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";
+import * as resources from "@pulumi/azure-native/resources";
 
 // Create a separate resource group for this example.
-const resourceGroup = new azure.core.ResourceGroup("cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg");
+const resourceGroup = new resources.ResourceGroup("cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg");
