fae1bf999bae65e20b075be5623472273d62b28f
Sebastian Racs <sebastian.racs@itk-engineering.de>
2022-06-02 15:30:31 +0200

    Add resource group

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
+
+import * as resources from "@pulumi/azure-native/resources";
+
+// Create a separate resource group for this example.
+const resourceGroup = new resources.ResourceGroup("cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg");
