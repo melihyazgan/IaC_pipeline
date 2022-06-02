a7570111e4a40bd701a94c01804734e2ce9eb872
Sebastian Racs <sebastian.racs@itk-engineering.de>
2022-06-02 15:33:50 +0200

    Use azure classic

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-
-import * as resources from "@pulumi/azure-native/resources";
+import * as azure from "@pulumi/azure";
 
 // Create a separate resource group for this example.
-const resourceGroup = new resources.ResourceGroup("cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg");
+const resourceGroup = new azure.core.ResourceGroup("cop-mya1rlz-maa2plg-caj1rlz-ras1rlz-container-reg");
